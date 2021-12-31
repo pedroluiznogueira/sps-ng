4f6bece19c0adf5c36a977e0663273d87cc10609
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-31 16:44:47 -0300

    implementing mocked token for auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './components/account/guard/auth.guard';
 import { LoginComponent } from './components/account/login/login.component';
 import { RegisterComponent } from './components/account/register/register.component';
 import { RepoComponent } from './components/entity/repo/repo.component';
@@ -11,19 +13,25 @@ const routes: Routes = [
     path: '', component: HomeComponent,
     children: [
       { path: '', component: RepoComponent }
-    ]
+    ],
+    canActivate: [AuthGuard]
   },
   {
     path: '', component: AuthenticationComponent,
     children: [
-      { path: '', component: LoginComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full'},
+      { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent }
     ]
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule,
+    [RouterModule.forRoot(routes)]
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/account/guard/auth.guard.ts
+++ src/app/components/account/guard/auth.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(
+    private router: Router
+  ) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree{
+    let mockedToken = window.sessionStorage.getItem('mock');
+
+    if (true) {
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
   }
   
 }
