099d00c4605749cfa2ccbcfa4469bb43914f25cb
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-31 15:19:40 -0300

    setted up project's folder architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { RegisterComponent } from './components/account/register/register.component';
+import { HomeComponent } from './components/layout/home/home.component';
+import { AuthenticationComponent } from './components/layout/authentication/authentication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
