abbb44257b72d794d8b6792a6a5edf86a220cfea
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-31 18:49:54 -0300

    logout functionallity to navbar logout button

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public logout(): void {
+    window.sessionStorage.clear();
+    this.redirectOut();        
+  }
+
+  public redirectOut(): void {
+    this.router.navigate(['login']);
   }
 
 }
