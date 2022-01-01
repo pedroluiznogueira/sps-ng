45eadc17c065452cf5bc7ebe4e477fbcb55e0f5c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-01 18:50:59 -0300

    user registration through api implemented

--- src/app/components/account/register/register.component.ts
+++ src/app/components/account/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
+import { User } from './../../../models/user/User';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  name?: string;
+  email?: string;
+  password?: string;
+  user?: User = new User();
+
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public register(): void {
+    this.user!.name = this.name;
+    this.user!.email = this.email;
+    this.user!.password = this.password;
+
+    this.userService.register(this.user!);
   }
 
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/models/user/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  url?: string = 'http://localhost:8080';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public register(user: User): void {
+    console.log(user);    
+    let obs = this.http.post(`${this.url}/users/insert`, user);
+    obs
+      .subscribe(
+        (resp) => {
+          console.log(resp);
+        }
+      );
+  }
 }
