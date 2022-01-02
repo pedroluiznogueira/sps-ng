512a0fe7c39e46e75a7762a887bafe80bcf578b2
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-01 21:17:43 -0300

    displaying feedback with loader animations when registering and logging in the platform

--- src/app/components/account/register/register.component.ts
+++ src/app/components/account/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user/user.service';
 import { User } from './../../../models/user/User';
 
@@ -13,9 +14,12 @@ export class RegisterComponent implements OnInit {
   email?: string;
   password?: string;
   user?: User = new User();
+  hideButton: boolean = true;
+  hideLoader: boolean = true;
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -25,8 +29,45 @@ export class RegisterComponent implements OnInit {
     this.user!.name = this.name;
     this.user!.email = this.email;
     this.user!.password = this.password;
+    this.displayLoader();
 
-    this.userService.register(this.user!);
+    this.userService.register(this.user!)
+      .then(
+        () => {
+          this.displaySuccessFeedback();
+    
+          setTimeout(() => {
+            this.registrationSucceded();
+          }, 1000);
+        })
+      .catch(
+        (error) => {
+          this.displayErrorFeedback();
+    
+          setTimeout(() => {
+            this.router.navigate(['/register']);
+          }, 1000);
+        }
+      );
+  }
+
+  public displayLoader(): void {
+    this.hideButton = false;
+    document.getElementById("loader")!.classList.add("loader");
+  }
+
+  public displaySuccessFeedback(): void {
+    this.hideLoader = false;
+    document.getElementById("feedback")!.classList.add("loader-success");
+  }
+
+  public displayErrorFeedback(): void {
+    this.hideLoader = false;
+    document.getElementById("feedback")!.classList.add("loader-error");
+  }
+
+  public registrationSucceded(): void {
+    this.router.navigate(['/login']);
   }
 
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { UserDto } from 'src/app/models/dto/user-dto/UserDto';
 import { User } from 'src/app/models/user/User';
 
@@ -12,30 +13,26 @@ export class UserService {
   url?: string = 'http://localhost:8080';
 
   constructor(
-    private http: HttpClient,
-    private router: Router
+    private http: HttpClient
   ) { }
 
-  public register(user: User): void {
-    console.log(user);    
-    let obs = this.http.post(`${this.url}/users/insert`, user);
-    obs
-      .subscribe(
-        (resp) => {
-          console.log(resp);
-        }
-      );
+  public register(user: User): Promise<User | undefined> {
+    const promise = this.http.post(`${this.url}/users/insert`, user).toPromise();
+    return promise;
   }
 
-  public login(userDto: UserDto): void {    
-    let obs = this.http.post(`${this.url}/users/auth`, userDto);
-    obs
-      .subscribe(
-        (resp: any) => {
-          console.log(resp);
-          window.sessionStorage.setItem("token", resp.token);
-          this.router.navigate(['/']);          
-        }
-      );
+  public login(userDto: UserDto): Promise<UserDto | undefined> {    
+    const promise = this.http.post(`${this.url}/users/auth`, userDto).toPromise();
+    return promise;
   }
+
+  // private fetchData(){
+  //   const promise = this.httpClient.get(this.apiUrl).toPromise();
+  //   console.log(promise);  
+  //   promise.then((data)=>{
+  //     console.log("Promise resolved with: " + JSON.stringify(data));
+  //   }).catch((error)=>{
+  //     console.log("Promise rejected with " + JSON.stringify(error));
+  //   });
+  // }
 }
